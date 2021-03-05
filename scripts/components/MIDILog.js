e9cc028a771a058244d7d38845388941d0965a6c
Glitch (hello-webpage) <none>
2021-03-05 17:50:03 +0000

    🍫🖇 Checkpoint ./scripts/components/MIDILog.js:24872531/1767

--- scripts/components/MIDILog.js
+++ scripts/components/MIDILog.js
@@ -1,19 +1,23 @@
 /* global Tone, ReactDOM, React */
 const MIDILog = props => {
-  const [enable, setEnable] = React.useState(false);
+  const [enable, setEnable] = React.useState(true);
   const [log, setLog] = React.useState([]);
   const tail_length = 10; // limit length of log
-  
+
   React.useEffect(() => {
     const handleDeviceLog = m => {
-      console.log('getting message', m)
+      console.log("getting message", m);
+      log.push(m.data);
+      // log.shift();
+      setLog([...log]);
+    };
+
+    if (props.device) {
+      props.device.addEventListener("midimessage", handleDeviceLog);
+      return () =>
+        props.device.removeEventListener("midimessage", handleDeviceLog);
     }
-    
-    if(props.device) {
-      
-    }
-      props.device.addEventListener('midimessage', handleDeviceLog);
-  }, [props.device]);
+  }, [props.device, log, setLog]);
 
   return (
     <div className="MIDILog">
