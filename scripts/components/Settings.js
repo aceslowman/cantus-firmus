d5d357b5499be8bd1618a79fe1466f3415bae87c
Glitch (hello-webpage) <none>
2021-03-13 04:35:41 +0000

    👌🎂 Checkpoint ./style.css:24872531/6691 ./scripts/components/Settings.js:24872531/26153

--- scripts/components/Settings.js
+++ scripts/components/Settings.js
@@ -14,7 +14,8 @@ const Settings = props => {
   return (
     <div className="SETTINGS" style={{ width: expanded ? "28%" : "0%" }}>
       <div className="settingsInner">
-        <div className="inputGroup">
+        <div className="inputSection">
+          <h3>basic</h3>
           <label>number of bars</label>
           <input
             onChange={props.onNumBarsChange}
@@ -23,8 +24,7 @@ const Settings = props => {
             step="1"
             value={props.numBars}
           />
-        </div>
-        <div className="inputGroup">
+
           <label>tempo</label>
           <input
             onChange={props.onBPMChange}
@@ -34,21 +34,19 @@ const Settings = props => {
             value={props.bpm}
           />
         </div>
-        <div className="inputGroup">
-          <label>
-            loop
-            <input
-              onChange={props.onToggleLoop}
-              checked={props.loop}
-              className="loopButton"
-              type="checkbox"
-            />
-          </label>
-        </div>
         <div className="inputSection">
           <h3>generate</h3>
+          
           <div className="buttonGroup">
-            <label htmlFor="jitter_amount">amount</label>
+            <label htmlFor="">key</label>
+            <select>
+              <option value="major">major</option>
+              
+            </select>
+          </div>
+
+          <label htmlFor="jitter_amount">amount</label>
+          <div className="buttonGroup">
             <input
               id="jitter_amount"
               onChange={props.onJitterAmountChange}
@@ -62,18 +60,6 @@ const Settings = props => {
               random jitter
             </button>
           </div>
-        </div>
-        <div className="buttonGroup">
-          <button
-            onClick={props.onTogglePlay}
-            className="playButton"
-            style={{
-              color: props.isPlaying ? "#fff" : "#602500",
-              backgroundColor: props.isPlaying ? "#602500" : "#fff"
-            }}
-          >
-            {props.isPlaying ? "stop" : "play"}
-          </button>
         </div>
         {/* <div className="inputGroup">
           <label htmlFor="midiinputs">Midi Input</label>
@@ -94,24 +80,51 @@ const Settings = props => {
           </select>
           <MIDILog device={props.activeMidiInput} />
         </div>*/}
-        <div className="inputGroup">
-          <label htmlFor="midioutputs">Midi Output</label>
-          <select
-            name="midioutputs"
-            value={props.activeMidiOutput ? props.activeMidiOutput.id : ""}
-            onChange={props.onMidiOutputChange}
+        <div className="inputSection">
+          <h3>MIDI</h3>
+          <div className="inputGroup">
+            <label htmlFor="midioutputs">Midi Output</label>
+            <select
+              name="midioutputs"
+              value={props.activeMidiOutput ? props.activeMidiOutput.id : ""}
+              onChange={props.onMidiOutputChange}
+            >
+              <option>select an output</option>
+              {props.midiOutputs &&
+                Object.keys(props.midiOutputs).map(e => {
+                  return (
+                    <option key={e} value={props.midiOutputs[e].id}>
+                      {props.midiOutputs[e].name}
+                    </option>
+                  );
+                })}
+            </select>
+            <MIDILog device={props.activeMidiInput} />
+          </div>
+        </div>
+
+        <div className="buttonGroup">
+          <div className="inputGroup">
+            <label>
+              loop
+              <input
+                onChange={props.onToggleLoop}
+                checked={props.loop}
+                className="loopButton"
+                type="checkbox"
+              />
+            </label>
+          </div>
+          <button
+            onClick={props.onTogglePlay}
+            className="playButton"
+            style={{
+              color: props.isPlaying ? "#fff" : "#602500",
+              backgroundColor: props.isPlaying ? "#602500" : "#fff"
+            }}
           >
-            <option>select an output</option>
-            {props.midiOutputs &&
-              Object.keys(props.midiOutputs).map(e => {
-                return (
-                  <option key={e} value={props.midiOutputs[e].id}>
-                    {props.midiOutputs[e].name}
-                  </option>
-                );
-              })}
-          </select>
-          <MIDILog device={props.activeMidiInput} />
+            {props.isPlaying ? "stop" : "play"}
+          </button>
         </div>
       </div>
       <div className="credits">cantus firmus by aceslowman 2021</div>
